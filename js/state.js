b59f3e713dd89b8df893d155b64d80c4fd34517b
DaniilNepomnyashchy <mr.daniil33@gmail.com>
2026-06-22 03:47:15 +0600

    chore: update README.md

--- js/state.js
+++ js/state.js
@@ -3,53 +3,17 @@ const state = {
     {
       id: "toDo",
       title: "To Do",
-      cards: [
-        {
-          id: "card_1712345678901", // уникальный ID через Date.now()
-          title: "Написать этот проект",
-          description:
-            "Вот сейчас пишу его. Сложный он конечно, но будем стараться, чего делать то блин емае.",
-          priority: "high", // 'low' | 'medium' | 'high'
-          tags: ["frontend", "figma", "algoritmes", "events"], // массив строк
-          createdAt: 1712345678901,
-        },
-        {
-          id: "card_1712124678901", // уникальный ID через Date.now()
-          title: "Сходить за кофе",
-          description: "Вкусный сырный латте, ммммм",
-          priority: "medium", // 'low' | 'medium' | 'high'
-          tags: ["coffee", "enjoyment"], // массив строк
-          createdAt: 1712345678901,
-        },
-      ],
+      cards: [],
     },
     {
       id: "inProgress",
       title: "In Progress",
-      cards: [
-        {
-          id: "card_1712345678231", // уникальный ID через Date.now()
-          title: "Написать обработку событий",
-          description: "Пора приступать за event.js",
-          priority: "medium", // 'low' | 'medium' | 'high'
-          tags: ["events", "clicks"], // массив строк
-          createdAt: 1712345678901,
-        },
-      ],
+      cards: [],
     },
     {
       id: "done",
       title: "Done",
-      cards: [
-        {
-          id: "card_1712345672431", // уникальный ID через Date.now()
-          title: "Реализовать перенос мышкой",
-          description: "Drag and drop в деле",
-          priority: "low", // 'low' | 'medium' | 'high'
-          tags: ["drag", "drop", "frontend"], // массив строк
-          createdAt: 1712345678901,
-        },
-      ],
+      cards: [],
     },
   ],
   filter: {
